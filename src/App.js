cb619c600716a79679974d8f8e3f9d7fbc7b0534
trexrush <edmazuera@gmail.com>
2021-07-19 01:42:18 -0500

    Added features component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import LandingPage from './views/LandingPage';
+import Features from './components/Features';
 
 function App() {
   return (
-      <LandingPage />
+      <>
+        <LandingPage />
+        <Features />
+      </>
   );
 }
 
---
+++ src/components/Features.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Features extends React.Component {
+    render() {
+        return <div className="features">
+            <h1>Features include:
+                <ul>
+                    <li>displaying a math problem</li>
+                    <li>showing a possible answer</li>
+                    <li>showing correct math answers</li>
+                    <li>helping people learn math</li>
+                </ul>
+            </h1>
+        </div>
+    }
+}
+
+export default Features;
+        
+        
+        
