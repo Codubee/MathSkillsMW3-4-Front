926e9f405cce2d8125c287d0d6cb230aae0fa657
Jocelyn Heckenkamp <jocelynheckenkamp@gmail.com>
2021-07-26 14:55:44 -0500

    Update images

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,37 +1,40 @@
 import React, { Component } from 'react';
 import Member from './Member';
-import jocelyn from './imgs/jocelyn.jpg';
 
 class Team extends Component {
     render() {
         const frontEnd = [
             {
                 name: "Ayush",
+                img: "https://m.media-amazon.com/images/I/61U7aD1GDML._AC_SS450_.jpg",
                 university: "University of Texas at Dallas",
                 level: "unknown level",
                 github: ""
             },
             {
                 name: "Eduardo",
+                img: "https://cdn.discordapp.com/attachments/867627997413376000/867628923587526666/IMG-20210109-WA0003_2.jpg",
                 university: "University of Texas at Dallas",
-                level: "unknown level",
-                github: "www.google.com"
+                level: "Junior",
+                github: "https://github.com/trexrush"
             },
             {
                 name: "Jocelyn",
-                img: "jocelyn.jpg",
+                img: "https://cdn.discordapp.com/attachments/858807182206959649/869304730738884659/img_4.png",
                 university: "University of Texas at Dallas",
                 level: "Sophomore",
                 github: "https://github.com/JocelynHeckenkamp"
             },
             {
                 name: "Quy",
+                img: "https://cdn.discordapp.com/attachments/858807182206959649/869304725839970314/img_1.png",
                 university: "University of Texas at Dallas",
                 level: "Junior",
                 github: "https://github.com/tqyn117"
             },
             {
                 name: "Riya",
+                img: "https://cdn.discordapp.com/attachments/858807182206959649/869304724254511115/img.png",
                 university: "University of Texas at Dallas",
                 level: "Sophomore",
                 github: "https://github.com/rng190001"
@@ -41,24 +44,28 @@ class Team extends Component {
         const backEnd = [
             {
                 name: "Jason",
+                img: "https://m.media-amazon.com/images/I/61U7aD1GDML._AC_SS450_.jpg",
                 university: "university unknown",
                 level: "level unknown",
                 github: ""
             },
             {
                 name: "Jesus",
+                img: "https://cdn.discordapp.com/attachments/858807182206959649/869304728125861948/img_2.png",
                 university: "University of Texas at Dallas",
                 level: "Senior",
                 github: "https://github.com/Martje55555"
             },
             {
                 name: "Salman",
+                img: "https://cdn.discordapp.com/attachments/858807182206959649/869304729124106310/img_3.png",
                 university: "University of Texas at Dallas",
                 level: "Sophomore",
                 github: "https://github.com/salmanh123"
             },
             {
                 name: "Suraj",
+                img: "https://m.media-amazon.com/images/I/61U7aD1GDML._AC_SS450_.jpg",
                 university: "University of Texas at Dallas",
                 level: "Junior",
                 github: ""
@@ -71,8 +78,8 @@ class Team extends Component {
                 <h2>Front-End</h2>
                 {frontEnd.map(m => <Member
                     name={m.name}
-                    img={m.img}
                     img="https://m.media-amazon.com/images/I/61U7aD1GDML._AC_SS450_.jpg"
+                    img={m.img}
                     university={m.university}
                     level={m.level}
                     github={m.github}
@@ -82,7 +89,6 @@ class Team extends Component {
                 {backEnd.map(m => <Member
                     name={m.name}
                     img={m.img}
-                    img="https://m.media-amazon.com/images/I/61U7aD1GDML._AC_SS450_.jpg"
                     university={m.university}
                     level={m.level}
                     github={m.github}
