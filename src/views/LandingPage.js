575026b1fd984004fe3ea1dd6d4d8e39db90eaff
codubee <info@codubee.com>
2021-07-26 15:48:14 -0500

    Moved the navbar into the app.js file

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import NavBar from '../components/Navbar';
 
 function LandingPage() 
 {
   return (
         <div>
-            <NavBar/>
             <h1>Math App</h1>
         </div>
     )
