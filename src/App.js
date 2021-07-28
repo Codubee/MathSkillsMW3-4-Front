2265fc492709860369819ab8a51bdedf2c036ae6
trexrush <edmazuera@gmail.com>
2021-07-28 15:55:46 -0500

    initial routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import Explanation from './components/Explanation';
-import LandingPage from './views/LandingPage';
-import NavBar from '../components/Navbar';
+// import LandingPage from './views/LandingPage';
+// import NavBar from '../components/Navbar';
+// import Features from './components/Features';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-      <div>
-        <NavBar/>
-        <LandingPage />
-      </div>
+      <Router>
+        <div>
+          {/* <NavBar/> */}
+          <Switch>
+            <Route path="/explanation">
+              <Explanation/>
+            </Route>
+            <Route path="/features">
+              {/* <Features /> */}
+            </Route>
+            <Route exact path="/">
+              <div>
+                Home
+              </div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
-
 export default App;
