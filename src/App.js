7cb6d2c5f4a9344f797f2d1afec03f483abb601a
trexrush <edmazuera@gmail.com>
2021-07-28 14:28:55 -0500

    requested changes for main branch merge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import LandingPage from './views/LandingPage';
 import Features from './components/Features';
 
 function App() {
   return (
-      <>
-        <LandingPage />
-        <Features />
-      </>
+      <Features />
   );
 }
 
--- src/components/Features.js
+++ src/components/Features.js
@@ -1,18 +1,17 @@
 import React from 'react'
 
-class Features extends React.Component {
-    render() {
-        return <div className="features">
-            <h1>Features include:
-                <ul>
-                    <li>displaying a math problem</li>
-                    <li>showing a possible answer</li>
-                    <li>showing correct math answers</li>
-                    <li>helping people learn math</li>
-                </ul>
-            </h1>
-        </div>
-    }
+function Features() {
+    return <div className="features">
+        <h1>Features include:
+            <ul>
+                <li>displaying a math problem</li>
+                <li>showing a possible answer</li>
+                <li>showing correct math answers</li>
+                <li>helping people learn math</li>
+            </ul>
+        </h1>
+    </div>
+
 }
 
 export default Features;
