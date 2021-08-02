0efdde11b069f5b1ffb05c6020283b9bed77c764
Jocelyn Heckenkamp <jocelynheckenkamp@gmail.com>
2021-08-02 11:02:37 -0500

    Weather component styling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Weather.css';
+import { WiThermometer } from 'react-icons/wi';
 
-class Weather extends React.Component {
-    render() {
-
-        return (
-            <div>
-                <p>Weather component!</p>
-            </div>
-        );
-    }
+function Weather() {
+    return (
+        <div className="weather-div">
+            <p>Right now it is</p>
+            <p className="weather-temperature">42</p>
+            <p>degrees in Dallas, TX</p>
+        </div>
+    );
 }
 
 export default Weather;
