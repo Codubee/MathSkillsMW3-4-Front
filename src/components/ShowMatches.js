dc68a54e54fecd85120ffb6c5ee2662f263ef03a
tqyn117 <quygiang2000@gmail.com>
2021-08-16 15:17:07 -0500

    Current status of the component

--- src/components/ShowMatches.js
+++ src/components/ShowMatches.js
@@ -2,25 +2,23 @@ import React from 'react';
 import { Table } from 'reactstrap';
 
 // This component is a function because it only needed to perform the task of displaying the matches
-function ShowMatches() {
-
-    //Creates a temporary array to test the ShowMatches function
-    const questions = [{problem:"2+2", answer:4},{problem:"3+3", answer:6}];
+function ShowMatches(props) {
 
     return (
         //This setups a striped table which makes it easier to keep track of
         <Table striped>
             <thead>
                 <tr>
+                    <th>ID</th>
                     <th>Problem</th>
                     <th>Answer</th>
                 </tr>
             </thead>
             <tbody>
                 {
-                    questions.map((question, id) => (
+                    props.matches.map((question, id) => (
                         <tr>
-                            <th scope="row">{id+=1}</th>
+                            <th>{question.problemId}</th>
                             <td>{question.problem}</td>
                             <td>{question.answer}</td>
                         </tr>
